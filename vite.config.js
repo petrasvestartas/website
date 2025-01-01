52f646263f1a12aacfd6e3bd7b2d3562d84f9359
petrasvestartas <petrasvestartas@gmail.com>
2025-01-01 20:00:51 +0100

    include dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/website/', // Add this line to set the base path
+  base: '/website/', // Add this line to set the base path
 
   plugins: [
     vue({
