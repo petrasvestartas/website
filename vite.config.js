7c28e8b063901de4b25f3993ab5be1a0b0b74eaf
petrasvestartas <petrasvestartas@gmail.com>
2025-01-01 23:07:20 +0100

    router

--- vite.config.js
+++ vite.config.js
@@ -4,39 +4,40 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/website/', // Add this line to set the base path
-  
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/website/' : '/', // Set the base path conditionally based on the mode
 
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          // Treat `swiper-container` and `swiper-slide` as custom elements
-          isCustomElement: (tag) => ['swiper-container'].includes(tag),
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            // Treat `swiper-container` and `swiper-slide` as custom elements
+            isCustomElement: (tag) => ['swiper-container', 'swiper-slide'].includes(tag),
+          }
         }
-      }
-    }),
-    vueDevTools(),
-  ],
+      }),
+      mode === 'development' && vueDevTools(), // Include vueDevTools only in development mode
+    ].filter(Boolean), // Filter out false values
 
-  build: {
-    sourcemap: true, // Enable source maps for your code
-    rollupOptions: {
-      output: {
-        sourcemapExcludeSources: true, // Exclude sources from source maps
+    build: {
+      sourcemap: true, // Enable source maps for your code
+      rollupOptions: {
+        output: {
+          sourcemapExcludeSources: true, // Exclude sources from source maps
+        },
       },
     },
-  },
-  server: {
-    sourcemap: false, // Enable source maps for development server
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  optimizeDeps: {
-    exclude: ['vue', 'three'], // Exclude specific dependencies from pre-bundling
-  },
+    server: {
+      sourcemap: false, // Enable source maps for development server
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    optimizeDeps: {
+      exclude: ['vue', 'three'], // Exclude specific dependencies from pre-bundling
+    },
+  }
 })
