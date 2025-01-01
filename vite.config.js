97e71c710250b266251cadda74fb8284024dfd83
petrasvestartas <petrasvestartas@gmail.com>
2025-01-01 20:22:40 +0100

    DEFAULT route

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/website/', // Add this line to set the base path
+  
 
   plugins: [
     vue({
