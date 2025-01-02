0545a74b4caa1a21941d312c07877517bf7055a2
petrasvestartas <petrasvestartas@gmail.com>
2025-01-02 18:41:05 +0100

    router

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/website/' : '/', // Set the base path conditionally based on the mode
+    // base: mode === 'production' ? '/website/' : '/', // Set the base path conditionally based on the mode
 
     plugins: [
       vue({
