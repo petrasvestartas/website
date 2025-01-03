d65717c2f6c4330b5571bd11219d975b8ba510eb
petrasvestartas <petrasvestartas@gmail.com>
2025-01-03 12:43:40 +0100

    move back

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { register } from 'swiper/element/bundle';
 import 'swiper/swiper-bundle.css';
 register();
 
-const test = 'test';
 
 import router from './router.js'; // Import the router instance
 import store from './store/index.js'; // Import the store instance
@@ -42,3 +41,47 @@ app.component('base-spinner', BaseSpinner);
 app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
+
+
+// import { createApp, defineAsyncComponent } from 'vue';
+
+// import { register } from 'swiper/element/bundle';
+// import 'swiper/swiper-bundle.css';
+// register();
+
+// import router from './router.js'; // Import the router instance
+// import store from './store/index.js'; // Import the store instance
+// import App from './App.vue';
+// import BaseCard from './components/ui/BaseCard.vue';
+// import BaseButton from './components/ui/BaseButton.vue';
+// import BaseBadge from './components/ui/BaseBadge.vue';
+// import BaseSpinner from './components/ui/BaseSpinner.vue';
+// import BaseDialog from './components/ui/BaseDialog.vue';
+// // const BaseDialog = defineAsyncComponent(() => import('./components/ui/BaseDialog.vue'));
+
+// import { initializeApp, getApps } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
+
+// function initializeFirebase(config) {
+//   let firebaseApp;
+//   if (!getApps().length) {
+//     firebaseApp = initializeApp(config);
+//   } else {
+//     firebaseApp = getApps()[0];
+//   }
+//   return getFirestore(firebaseApp);
+// }
+
+// const app = createApp(App);
+// app.config.globalProperties.$initializeFirebase = initializeFirebase;
+
+
+// app.use(router); // Use the router instance
+// app.use(store); // Use the store instance
+// app.component('base-card', BaseCard);
+// app.component('base-button', BaseButton);
+// app.component('base-badge', BaseBadge);
+// app.component('base-spinner', BaseSpinner);
+// app.component('base-dialog', BaseDialog);
+
+// app.mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHashHistory } from 'vue-router'; // Use createWebHashHistory instead of createWebHistory
+import { defineAsyncComponent } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router'; // Import the createRouter function
 
 import ProjectDetail from './pages/projects/ProjectsDetail.vue';
 import ProjectsList from './pages/projects/ProjectsList.vue';
@@ -13,29 +14,44 @@ import Papers from './pages/papers/Papers.vue';
 import About from './pages/about/About.vue';
 import store from './store/index.js';
 
+// const ProjectDetail = defineAsyncComponent(() => import('./pages/projects/ProjectsDetail.vue'));
+// const ProjectRegistration = defineAsyncComponent(() => import('./pages/projects/ProjectRegistration.vue'));
+// const ContactProject = defineAsyncComponent(() => import('./pages/requests/ContactProject.vue'));
+// const RequestsReceived = defineAsyncComponent(() => import('./pages/requests/RequestsReceived.vue'));
+// const UserAuth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'));
+// const Code = defineAsyncComponent(() => import('./pages/code/Code.vue'));
+// const Teaching = defineAsyncComponent(() => import('./pages/teaching/Teaching.vue'));
+// const Papers = defineAsyncComponent(() => import('./pages/papers/Papers.vue'));
+// const About = defineAsyncComponent(() => import('./pages/about/About.vue'));
+
+
+
+
 const router = createRouter({
-    history: createWebHashHistory(), // Use createWebHashHistory
+    history: createWebHistory(), // Use the createWebHistory
     routes: [
         { path: '/', redirect: '/projects' },
-        { path: '/website', redirect: '/projects' }, // Redirect /website to /projects
         { path: '/projects', component: ProjectsList },
         { 
             path: '/projects/:id', 
             component: ProjectDetail,
             props: true, // Pass the route params as props to the component
             children: [
-                { path: 'contact', component: ContactProject }, 
-            ] 
-        },
+            { path: 'contact', component: ContactProject }, 
+        ] },
         { path: '/contact', component: ContactProject },
         { path: '/register', component: ProjectRegistration, meta: { requiresAuth: true } },
         { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
         { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
         { path: '/:notFound(.*)', component: NotFound },
-        { path: '/code', component: Code },
-        { path: '/teaching', component: Teaching },
-        { path: '/papers', component: Papers },
-        { path: '/about', component: About },
+        { path: '/code', component: Code},
+        { path: '/teaching', component: Teaching},
+        { path: '/papers', component: Papers},
+        { path: '/about', component: About},
+
+
+
+
     ], // Add the routes array here
 }); // Create a new router instance
 
@@ -50,3 +66,56 @@ router.beforeEach((to, _, next) => {
 }); // Add a global navigation guard
 
 export default router; // Export the router instance
+
+// import { createRouter, createWebHashHistory } from 'vue-router'; // Use createWebHashHistory instead of createWebHistory
+
+// import ProjectDetail from './pages/projects/ProjectsDetail.vue';
+// import ProjectsList from './pages/projects/ProjectsList.vue';
+// import ProjectRegistration from './pages/projects/ProjectRegistration.vue';
+// import ContactProject from './pages/requests/ContactProject.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
+// import NotFound from './pages/NotFound.vue';
+// import Code from './pages/code/Code.vue';
+// import Teaching from './pages/teaching/Teaching.vue';
+// import Papers from './pages/papers/Papers.vue';
+// import About from './pages/about/About.vue';
+// import store from './store/index.js';
+
+// const router = createRouter({
+//     history: createWebHashHistory(), // Use createWebHashHistory
+//     routes: [
+//         { path: '/', redirect: '/projects' },
+//         { path: '/website', redirect: '/projects' }, // Redirect /website to /projects
+//         { path: '/projects', component: ProjectsList },
+//         { 
+//             path: '/projects/:id', 
+//             component: ProjectDetail,
+//             props: true, // Pass the route params as props to the component
+//             children: [
+//                 { path: 'contact', component: ContactProject }, 
+//             ] 
+//         },
+//         { path: '/contact', component: ContactProject },
+//         { path: '/register', component: ProjectRegistration, meta: { requiresAuth: true } },
+//         { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+//         { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+//         { path: '/:notFound(.*)', component: NotFound },
+//         { path: '/code', component: Code },
+//         { path: '/teaching', component: Teaching },
+//         { path: '/papers', component: Papers },
+//         { path: '/about', component: About },
+//     ], // Add the routes array here
+// }); // Create a new router instance
+
+// router.beforeEach((to, _, next) => {
+//     if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+//         next('/auth');
+//     } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+//         next('/projects');
+//     } else {
+//         next();
+//     }
+// }); // Add a global navigation guard
+
+// export default router; // Export the router instance
